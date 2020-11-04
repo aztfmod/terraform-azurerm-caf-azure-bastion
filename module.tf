5ce8fee3d19a02b1980850bb62d65d0bec395049
Benoit-LAGUET <laguet.benoit@gmail.com>
2020-11-04 14:11:46 +0100

    fix: bastion name use the variable
    This bug came from the the caf fundation project that use a object named "bastion_config"

--- module.tf
+++ module.tf
@@ -1,5 +1,5 @@
 resource "azurerm_bastion_host" "azurebastion" {
-  name                = var.bastion_config.name
+  name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
   tags                = var.tags
